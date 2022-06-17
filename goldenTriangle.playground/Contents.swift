c76e3d580475426c22522682b39e1486fbd8bca0
mteera <mongkol.teera@accenture.com>
2022-06-17 15:25:20 +0700

    Updated comment

--- goldenTriangle.playground/Contents.swift
+++ goldenTriangle.playground/Contents.swift
@@ -5,7 +5,7 @@ func hasGoldenTriangle(_ values: [Int]) -> Bool {
     }
     // Sort array
     let sortedArray = values.sorted()
-    // Sum up each sides while looping just once
+    // Iterate through all 3 consecutive triplets
     for i in 0...sortedArray.count where i < sortedArray.count - 2 {
         if sortedArray[i] + sortedArray[i + 1] > sortedArray[i + 2],
            sortedArray[i] + sortedArray[i + 2] > sortedArray[i + 1],
